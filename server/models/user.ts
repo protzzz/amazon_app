e5e8f89200df1f93adc67a4e89f7f6d6bd70021e
Bohdan <protzzz.it@gmail.com>
2025-09-10 12:50:29 +0200

    Create user schema in mongo
    Email with use of regular expression check

---
+++ server/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        const regex =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(regex);
+      },
+      // Message will show if match returns false
+      message: "Please enter a valid email address.",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "user",
+  },
+});
