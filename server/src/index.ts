6df715e42b2b07aac3fdf5e68c2eb0e4b141b734
Bohdan <protzzz.it@gmail.com>
2025-09-11 19:41:09 +0200

    Add MongoDB connection and auth router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,27 @@
+// IMPORT FROM PACKAGES
 import express from "express";
+import mongoose from "mongoose";
 
+// IMPORT FROM FILES
+import authRouter from "./routes/auth";
+
+// INIT
 const app = express();
 const PORT = 3000;
+const DB_URI = "";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
