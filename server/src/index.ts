d62710e8a54e130cf8eeb30531af28468cd2021e
Bohdan <protzzz.it@gmail.com>
2025-09-11 21:07:21 +0200

    Add dotenv for MongoDB URI configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,21 @@
 // IMPORT FROM PACKAGES
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 // IMPORT FROM FILES
 import authRouter from "./routes/auth";
 
 // INIT
+dotenv.config();
 const app = express();
 const PORT = 3000;
-const DB_URI = "";
+const DB_URI = `mongodb+srv://${process.env.MONGO_DB_URI_NAME}:${process.env.MONGO_DB_URI_PASSWORD}@cluster0.mtkozwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// console.log("ENV USER:", process.env.MONGO_DB_URI_NAME);
+// console.log("ENV PASS LEN:", (process.env.MONGO_DB_URI_PASSWORD || "").length);
+
+mongoose.connect(DB_URI);
 
 // middleware
 app.use(express.json());
