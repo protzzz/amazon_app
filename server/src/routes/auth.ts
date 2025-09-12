cd631a2ec1e0d55f1454d157be31a142c21ad422
Bohdan <protzzz.it@gmail.com>
2025-09-12 14:33:18 +0200

    Add signin route with JWT authentication

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,6 +2,7 @@
 import { Router, Request, Response } from "express";
 import User from "../models/user";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
@@ -9,12 +10,12 @@ authRouter.post("/api/signup", async (req: Request, res: Response) => {
   try {
     // get the data from client (json body)
     /* default json body
-  {
-    'name': name,
-    'email': email,
-    'password': password,
-  }
-  */
+    {
+      'name': name,
+      'email': email,
+      'password': password,
+    }
+    */
     const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -25,17 +26,46 @@ authRouter.post("/api/signup", async (req: Request, res: Response) => {
 
     const hashedPassword = await bcryptjs.hash(password, 8);
 
+    // create a new user
     let user = new User({
       email,
       password: hashedPassword,
       name,
     });
 
-    user = await user.save();
-    res.json(user);
-
     // post that data in database
+    user = await user.save();
+
     // return that data to the user
+    res.json(user);
+  } catch (error) {
+    const message = error instanceof Error ? error.message : String(error);
+    res.status(500).json({ error: message });
+  }
+});
+
+authRouter.post("/api/signin", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email addres does not exist!" });
+    }
+
+    const isMatchPassword = await bcryptjs.compare(
+      password,
+      existingUser.password
+    );
+    if (!isMatchPassword) {
+      return res.status(400).json({ message: "Incorrect password!" });
+    }
+
+    const token = jwt.sign({ id: existingUser._id }, "passwordKey");
+
+    res.json({ token, ...existingUser });
   } catch (error) {
     const message = error instanceof Error ? error.message : String(error);
     res.status(500).json({ error: message });
