1ec165db78ce1463c26980746d062e50cd7f49ca
Bohdan <protzzz.it@gmail.com>
2025-09-12 12:47:31 +0200

    Improve .env loading error handling in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:amazon_clone_app/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+// Future<void> main() async {
+//   await dotenv.load(fileName: ".env");
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    throw Exception('Error loading .env file: $e');
+  }
   runApp(const MyApp());
 }
 
