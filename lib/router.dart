9a745de423666dbef1b9ae36e34dde414ff605cf
Bohdan <protzzz.it@gmail.com>
2025-09-13 03:13:33 +0200

    Add HomePage route to router

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone_app/features/auth/pages/auth_page.dart';
+import 'package:amazon_clone_app/features/home/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
@@ -8,12 +9,17 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: (_) => AuthPage(),
       );
+    case HomePage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => HomePage(),
+      );
     default:
       return MaterialPageRoute(
         settings: routeSettings,
         builder:
             (_) => Scaffold(
-              body: Center(child: Text('Screen does not exist!')),
+              body: Center(child: Text('Page does not exist!')),
             ),
       );
   }
