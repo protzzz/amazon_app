bfbb3f8d8b68adc394a80910d0246612652e1d87
Bohdan <protzzz.it@gmail.com>
2025-09-13 03:19:08 +0200

    Refactor auth repository to use remote API

--- lib/features/auth/repository/auth_remote_repository.dart
+++ lib/features/auth/repository/auth_remote_repository.dart
@@ -1,12 +1,64 @@
 import 'dart:convert';
-import 'package:amazon_clone_app/core/constants/error_handling.dart';
 import 'package:amazon_clone_app/core/constants/global_variables.dart';
-import 'package:amazon_clone_app/core/constants/utils.dart';
-import 'package:amazon_clone_app/models/user.dart';
-import 'package:flutter/material.dart';
+import 'package:amazon_clone_app/core/services/shared_preferences_service.dart';
+import 'package:amazon_clone_app/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
-class AuthService {
+class AuthRemoteRepository {
+  final spServie = SharedPreferencesService();
+
+  Future<UserModel> signUp({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'name': name,
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      if (res.statusCode != 201) {
+        throw jsonDecode(res.body)['error'];
+      }
+
+      return UserModel.fromJson(res.body);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  Future<UserModel> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$uri/api/signin'),
+        body: jsonEncode({'email': email, 'password': password}),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (res.statusCode != 200) {
+        throw jsonDecode(res.body)['error'];
+      }
+
+      print(res.body);
+
+      return UserModel.fromJson(res.body);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  /*
   void signUpUser({
     required BuildContext context,
     required String email,
@@ -14,7 +66,7 @@ class AuthService {
     required String name,
   }) async {
     try {
-      User user = User(
+      UserModel user = UserModel(
         id: 'id',
         name: name,
         email: email,
@@ -46,7 +98,9 @@ class AuthService {
       showSnackbar(context, e.toString());
     }
   }
+  */
 
+  /*
   void signInUser({
     required BuildContext context,
     required String email,
@@ -66,10 +120,13 @@ class AuthService {
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () {},
+        onSuccess: () async {
+          await spServie.setToken(jsonDecode(res.body)['token']);
+        },
       );
     } catch (e) {
       showSnackbar(context, e.toString());
     }
   }
+  */
 }
