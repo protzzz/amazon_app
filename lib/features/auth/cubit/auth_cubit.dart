cea757d8a5035fb2421794a29d30af693c0707cf
protzzz <99717913+protzzz@users.noreply.github.com>
2025-09-17 00:35:22 +0200

    Implemented AuthCubit with signUp and login methods

---
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -0,0 +1,55 @@
+import 'package:amazon_clone_app/core/services/shared_preferences_service.dart';
+import 'package:amazon_clone_app/features/auth/repository/auth_remote_repository.dart';
+import 'package:amazon_clone_app/models/user_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+
+  final authRemoteRepository = AuthRemoteRepository();
+  final spService = SharedPreferencesService();
+
+  void signUp({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      emit(AuthLoading());
+
+      await authRemoteRepository.signUp(
+        email: email,
+        password: password,
+        name: name,
+      );
+
+      emit(AuthSignUp());
+    } catch (error) {
+      emit(AuthError(error.toString()));
+    }
+  }
+
+  void login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+
+      final user = await authRemoteRepository.login(
+        email: email,
+        password: password,
+      );
+
+      if (user.token.isNotEmpty) {
+        await spService.setToken(user.token);
+      }
+
+      emit(AuthLoggedIn(user));
+    } catch (error) {
+      emit(AuthError(error.toString()));
+    }
+  }
+}
