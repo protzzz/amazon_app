fdfde5537d08df9c1eb5bc303c3874a02b781f8d
protzzz <99717913+protzzz@users.noreply.github.com>
2025-09-18 15:21:23 +0200

    Add SignUpBody and LoginBody interfaces for handling request bodies in auth routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -7,72 +7,89 @@ import { error } from "console";
 
 const authRouter = Router();
 
-authRouter.post("/api/signup", async (req: Request, res: Response) => {
-  try {
-    // get the data from client (json body)
-    /* default json body
+interface SignUpBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+authRouter.post(
+  "/signup",
+  async (req: Request<{}, {}, SignUpBody>, res: Response) => {
+    try {
+      // get the data from client (json body)
+      /* default json body
     {
       'name': name,
       'email': email,
       'password': password,
     }
     */
-    const { name, email, password } = req.body;
+      const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+      const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with the same email already exists!" });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ message: "User with the same email already exists!" });
+      }
+
+      const hashedPassword = await bcryptjs.hash(password, 8);
+
+      // create a new user
+      let user = new User({
+        email,
+        password: hashedPassword,
+        name,
+      });
+
+      // post that data in database
+      user = await user.save();
+
+      // return that data to the user
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
-
-    const hashedPassword = await bcryptjs.hash(password, 8);
-
-    // create a new user
-    let user = new User({
-      email,
-      password: hashedPassword,
-      name,
-    });
-
-    // post that data in database
-    user = await user.save();
-
-    // return that data to the user
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ error: error });
   }
-});
+);
 
-authRouter.post("/api/signin", async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
+authRouter.post(
+  "/login",
+  async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    try {
+      const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+      const existingUser = await User.findOne({ email });
 
-    if (!existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with this email addres does not exist!" });
+      if (!existingUser) {
+        return res
+          .status(400)
+          .json({ message: "User with this email addres does not exist!" });
+      }
+
+      const isMatchPassword = await bcryptjs.compare(
+        password,
+        existingUser.password
+      );
+      if (!isMatchPassword) {
+        return res.status(400).json({ error: "Incorrect password!" });
+      }
+
+      const token = jwt.sign({ id: existingUser._id }, "passwordKey");
+
+      res.json({ token, ...existingUser });
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
-
-    const isMatchPassword = await bcryptjs.compare(
-      password,
-      existingUser.password
-    );
-    if (!isMatchPassword) {
-      return res.status(400).json({ error: "Incorrect password!" });
-    }
-
-    const token = jwt.sign({ id: existingUser._id }, "passwordKey");
-
-    res.json({ token, ...existingUser });
-  } catch (error) {
-    res.status(500).json({ error: error });
   }
-});
+);
 
 authRouter.post("/tokenIsValid", async (req: Request, res: Response) => {
   try {
